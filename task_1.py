65de468fb4f025af91ecb9069e2f85bdc4857504
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2025-04-18 23:04:35 +0530

    rectify output formatting

--- task_1.py
+++ task_1.py
@@ -1,12 +1,12 @@
 print("\tFind even & odd from your given input\n")
 
 try:
-    num = int(input("Enter a number to check even or odd: "))
+    num = int(input("Enter a number: "))
 
     if (num % 2 == 0):
-        print("\n%d is an even number" %(num))
+        print("%d is an even number" %(num))
     else:
-        print(f"\n{num} is an odd number")
+        print(f"{num} is an odd number")
 except ValueError:
     print("\nOnly integer number is accepted")
     print("Please run the program again to get correct output")
